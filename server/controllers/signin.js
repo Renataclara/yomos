47184c34255375eccdeb0c0a39f7ef02d90e98ba
tamastro <rpratama45@gmail.com>
2017-08-25 09:29:10 +0700

    hash

--- server/controllers/signin.js
+++ server/controllers/signin.js
@@ -1,18 +1,23 @@
 const users = require('../models/users');
 const jwt = require('jsonwebtoken');
+const random = require('../helpers/hash')
 
 exports.signin = (req, res) => {
   users.userModel.findOne({
-      username: req.body.username,
-      password: req.body.password
+      username: req.body.username
     })
     .then(data => {
-      var token = jwt.sign({
-        username: data.username
-      }, process.env.YOMOS_SECRET);
-      res.send(token)
+      pass = random.hashish(req.body.password, data.salt)
+      if (pass == data.password) {
+        var token = jwt.sign({
+          username: data.username
+        }, process.env.YOMOS_SECRET);
+        res.send(token)
+      } else {
+        res.send('wrong password')
+      }
     })
     .catch(err => {
-      res.send(err)
+      res.send('Username Not Exist')
     })
 }

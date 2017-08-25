08e077fb0a228a96b55895ca51ed22d0eee1a417
tamastro <rpratama45@gmail.com>
2017-08-25 13:53:07 +0700

    ayo coba benerin conflictnya

--- server/controllers/signup.js
+++ server/controllers/signup.js
@@ -1,12 +1,12 @@
-const users = require('../models/users');
+const users = require('../models/user');
 const random = require('../helpers/hash')
 
 exports.signup = (req, res) => {
   let secret = random.randomStr(8);
   users.userModel.create({
       username: req.body.username,
-      password: random.hashish(req.body.password, secret)
-      salt: salt
+      password: random.hashish(req.body.password, secret),
+      salt: secret
     })
     .then(data => {
       res.send(data)
--- server/models/user.js
+++ server/models/user.js
@@ -4,12 +4,12 @@ var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/yomos');
 
 var userSchema = mongoose.Schema({
-    usename: String,
+    username: String,
     password: String,
     salt: String
 })
 
-var userModel = mongoose.model('Books', userSchema);
+var userModel = mongoose.model('user', userSchema);
 
 
 module.exports = {
